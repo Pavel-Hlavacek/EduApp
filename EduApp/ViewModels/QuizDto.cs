95b36f241c35591132482430b286010df8c98c03
Pavel-Hlavacek <153230790+Pavel-Hlavacek@users.noreply.github.com>
2026-02-10 05:06:08 +0100

    Fixed bug - editing create new quiz

--- EduApp/ViewModels/QuizDto.cs
+++ EduApp/ViewModels/QuizDto.cs
@@ -10,5 +10,6 @@ namespace EduApp.ViewModels
         public string Title { get; set; }
         public string Description { get; set; }
         public List<QuestionDto> Questions { get; set; } = new();
+        public int Id { get; init; }
     }
 }
